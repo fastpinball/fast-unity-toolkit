601332158adf773b140d796d07f3e051300cf451
Linolium1 <megasonx@gmail.com>
2023-09-26 14:57:44 -0400

    Poor-Mans c# interface document reference

---
+++ Interface-Ref/FAST_Defs.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+#if GODOT
+using Godot;
+using EngineNodeAlias = Godot.Node3D;
+#elif UNITY_2020_3_OR_NEWER
+using UnityEngine;
+using EngineNodeAlias = UnityEngine.MonoBehaviour;
+#endif
+
+namespace FAST_Pinball
+{
+//------------------------------
+
+
+public partial class FAST: EngineNodeAlias
+{
+ public enum eResult
+ {
+	SUCCESS=0,
+	
+	// Connection
+	NO_HARDWARE_FOUND,
+	INCORRECT_CONFIGURATION,
+	
+	// Motors/Servos
+	LIMIT_NOT_FOUND,
+	AT_MAX_LIMIT,
+	AT_MIN_LIMIT,
+ }
+ 
+ public enum eExpansionDestinations
+   {
+	NEURON=0,
+	EXPANSION,
+   }
+   
+ public enum eNodeBoards
+   {
+	FP_CAB0001=0,
+	FP_IO0804,
+	FP_IO1616,
+	FP_IO3208,    
+   }
+   
+ public enum eExpansionBoards
+   {
+	// 128 LEDs
+	NEURON=0,
+	
+	// 128 LEDs, 4 Servos
+	FP_EXP0071,
+	
+	// 256 LEDs
+	FP_EXP0081,
+	
+	// 128 LEDs, 2 Breakouts
+	FP_EXP0091,
+   }
+   
+   
+ public enum eDriverPWM
+   {
+	PWM_0   = 0x00,
+	PWM_12  = 0x01,
+	PWM_25  = 0x88,
+	PWM_37  = 0x92,
+	PWM_50  = 0xAA,
+	PWM_62  = 0xBA,
+	PWM_75  = 0xEE,
+	PWM_87  = 0xFE,
+	PWM_100 = 0xFF,
+   }
+}
+
+//-----------------------------
+// namespace FAST_Pinball
+}
